75ec9ebe9874a5120b92b1bd57ab26f0d71598a3
Tayfun DOGANAY <73904292+tayfundoganay@users.noreply.github.com>
2022-09-13 23:19:15 +0300

    Update bankingSystem.cpp

--- bankingSystem.cpp
+++ bankingSystem.cpp
@@ -54,6 +54,80 @@ public:
 
 int main()
 {
+	Bank bank;
+	Account account;
+	int choice;
+	string fname, lname;
+	long accountNumber;
+	float balance;
+	float amount;
+	cout << "***TTT INVESTMENT BANKING***" << endl;
+	do
+	{
+		cout << "\n\tSelect one option below ";
+		cout << "\n\t1 Open an Account";
+		cout << "\n\t2 Balance Enquiry";
+		cout << "\n\t3 Deposit";
+		cout << "\n\t4 Withdrawal";
+		cout << "\n\t5 Close an Account";
+		cout << "\n\t6 Show All Accounts";
+		cout << "\n\t7 Quit";
+		cout << "\nEnter your choice: ";
+		cin >> choice;
+		switch (choice)
+		{
+		case 1:
+			cout << "Enter First Name: ";
+			cin >> fname;
+			cout << "Enter Last Name: ";
+			cin >> lname;
+			cout << "Enter initil Balance: ";
+			cin >> balance;
+			account = bank.OpenAccount(fname, lname, balance);
+			cout << endl << "Congradulation Account is Created" << endl;
+			cout << account;
+			break;
+		case 2:
+			cout << "Enter Account Number:";
+			cin >> accountNumber;
+			account = bank.BalanceEnquiry(accountNumber);
+			cout << endl << "Your Account Details" << endl;
+			cout << account;
+			break;
+		case 3:
+			cout << "Enter Account Number:";
+			cin >> accountNumber;
+			cout << "Enter Balance:";
+			cin >> amount;
+			account = bank.Deposit(accountNumber, amount);
+			cout << endl << "Amount is Deposited" << endl;
+			cout << account;
+			break;
+		case 4:
+			cout << "Enter Account Number:";
+			cin >> accountNumber;
+			cout << "Enter Balance:";
+			cin >> amount;
+			account = bank.Withdraw(accountNumber, amount);
+			cout << endl << "Amount Withdrawn" << endl;
+			cout << account;
+			break;
+		case 5:
+			cout << "Enter Account Number:";
+			cin >> accountNumber;
+			bank.CloseAccount(accountNumber);
+			cout << endl << "Account is Closed" << endl;
+			cout << account;
+		case 6:
+			bank.ShowAllAccounts();
+			break;
+		case 7: break;
+		default:
+			cout << "\nEnter corret choice";
+			exit(0);
+		}
+	} while (choice != 7);
+	return 0;
 }
 
 
